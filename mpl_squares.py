439405cae1d9fb1fce9fbf7cebd177298b8fcf30
Yauheni Neshyn <Neshyn.Z@gmail.com>
2020-11-25 21:42:19 +0300

    Change mpl squares plot

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-plt.plot(squares, linewidth=4)
+plt.plot(input_values, squares, linewidth=4)
 
 # Set chart title and label axes.
 plt.title("Square numbers", fontsize=22)
