515c5d808ed018858bacf106d736f8e3ed51d253
Yauheni Neshyn <Neshyn.Z@gmail.com>
2020-11-29 16:26:22 +0300

    Remove script with 8 sides dice

--- dice_visual.py
+++ dice_visual.py
@@ -4,8 +4,8 @@ import pygal
 
 def main():
     # Create two dice.
-    die_1 = Die()
-    die_2 = Die()
+    die_1 = Die(8)
+    die_2 = Die(8)
 
     # Make some rolls, and store results in a list.
     results = [die_1.roll() + die_2.roll() for _ in range(1000)]
@@ -17,12 +17,12 @@ def main():
     # Visualize the results.
     hist = pygal.Bar()
 
-    hist.title = "Results of rolling two D6 dice 1000 times."
-    hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+    hist.title = "Results of rolling two D8 dice 10,000,000 times."
+    hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
     hist.x_title = "Result"
     hist.y_title = "Frequency of Result"
 
-    hist.add('D6 + D6', frequencies)
+    hist.add('D8 + D8', frequencies)
     hist.render_to_file('dice_visual.svg')
 
 
