8fd04505d3d5ea6c75ef51c6ffb05d4eb5632c89
Yauheni Neshyn <Neshyn.Z@gmail.com>
2020-12-01 23:54:40 +0300

    Add lows temperature to highs_lows visualization

--- highs_lows.py
+++ highs_lows.py
@@ -4,18 +4,21 @@ from datetime import datetime
 
 
 def main():
-    # Get high temperatures from file.
+    # Get dates, high, low temperatures from file.
     filename = 'data/sitka_weather_2018_full.csv'
     with open(filename) as f:
         reader = csv.reader(f)
         header_row = next(reader)
 
-        highs = []
-        dates = []
+        highs, lows, dates = [], [], []
         for row in reader:
             try:
                 high = int(row[8])
                 highs.append(high)
+
+                low = int(row[9])
+                lows.append(low)
+
                 current_date = datetime.strptime(row[2], "%Y-%m-%d")
                 dates.append(current_date)
             except ValueError:
@@ -23,10 +26,12 @@ def main():
 
     # Plot data.
     fig = plt.figure(dpi=128, figsize=(10, 6))
-    plt.plot(dates, highs, c='red')
+    plt.plot(dates, lows, c='blue', alpha=0.5)
+    plt.plot(dates, highs, c='red', alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
     # Format plot.
-    plt.title("Daily high temperatures, 2018", fontsize=24)
+    plt.title("Daily high and low temperatures, 2018", fontsize=24)
     plt.xlabel('', fontsize=1)
     fig.autofmt_xdate()
     plt.ylabel('Temperature (F)', fontsize=16)
