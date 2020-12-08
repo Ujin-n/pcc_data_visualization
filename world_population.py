f98bfdaf517a1af60b3488d26f3bdb5329ac46a1
Yauheni Neshyn <Neshyn.Z@gmail.com>
2020-12-08 23:26:56 +0300

    Add some styling for world population plot

--- world_population.py
+++ world_population.py
@@ -1,6 +1,7 @@
 import json
 from country_codes import  get_country_code
 import pygal
+from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 
 # Load the data into a list.
 filename = 'data/population_data.json'
@@ -30,7 +31,9 @@ for cc, pop in cc_populations.items():
 # See how many countries are in each level.
 print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
 
-wm = pygal.maps.world.World()
+wm_style = RS('#336699', base_style=LCS)
+wm = pygal.maps.world.World(style=wm_style)
+
 wm.title = 'World Population in 2010, by Country'
 wm.add('0-10m', cc_pops_1)
 wm.add('10m-1bn', cc_pops_2)
